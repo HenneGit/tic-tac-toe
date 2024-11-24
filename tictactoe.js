4b4b73bc899d09a886f7cbb58ab10d3fb7fdb9be
Henne <henne@localhost.localdomain>
2024-11-24 17:35:27 +0100

    checking for win condition.

--- tictactoe.js
+++ tictactoe.js
@@ -2,7 +2,7 @@
 
 
     const game = [];
-    
+    let player = 'circle';
     document.addEventListener('DOMContentLoaded', () => {
         startGame();
     });
@@ -17,19 +17,60 @@
         const boardContainer = document.createElement('div');
         boardContainer.id = 'board-container';
         boardContainer.classList.add('board-container');
+        let rowNumber = 0;
+        let columnNumber = 0;
         for (let i = 0; i < 9; i++) {
+            if (columnNumber === 3) {
+                rowNumber++;
+                columnNumber = 0;
+            }
             let element = document.createElement('div');
             element.addEventListener('click', onFieldClick);
-            element.id = i + "-field-" + boardId;
-            element.classList.add('field')
+            element.id = rowNumber +  columnNumber +"-field-" + boardId;
+            element.classList.add('field', rowNumber + 'row', columnNumber + 'column');
+            columnNumber++;
             boardContainer.append(element);
         }
         root.append(boardContainer)
     };
     
     const onFieldClick = (event) => {
-        console.log(event.target.id);
+        player = player === 'circle' ? 'x' : 'circle';
+        let id = event.target.id;
+        let field = getElementById(id);
+        if (field.children.length > 0) {
+            return;
+        }
+        const icon = document.createElement('span');
+        if (player === "circle") {
+            icon.className = 'icon circle';
+        } else {
+            icon.className = 'icon x-mark';
+        }
+        field.append(icon);
+        checkWinningCondition(id);
     }
+
+
+
+
+    const checkWinningCondition = (id) => {
+
+        let currentField = getElementById(id);
+        getRow(currentField.id[0]);
+
+    };
+
+
+    const getRow = (rowNumber) => {
+        let board = getElementById('board-container');
+        const row1Elements = board.querySelectorAll('.' + rowNumber + 'row');
+        console.log(row1Elements);
+
+    };
+
+
+    const getColumnNumber = (id) => {    };
 
 
     const getElementById = (id) => {
